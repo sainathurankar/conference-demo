a4988cc5477402f6b41fb4dd2cbfdcefa03ffd46
U6064297 <sai.ura999@gmail.com>
2021-08-30 12:01:00 +0530

    third

--- src/main/java/com/example/demo/config/PersistenceConfiguration.java
+++ src/main/java/com/example/demo/config/PersistenceConfiguration.java
@@ -8,7 +8,7 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
+   /* @Bean
     public DataSource dataSource(){
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
@@ -16,5 +16,5 @@ public class PersistenceConfiguration {
         builder.password("sai");
         System.out.println("My custom datsource");
         return builder.build();
-    }
+    }*/
 }
